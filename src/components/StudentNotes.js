8a507401a540e3a61f987a8dbd6cbb715b80414f
BertCasale <bthebest55@gmail.com>
2023-02-21 20:17:03 -0500

    form layout

--- src/components/StudentNotes.js
+++ src/components/StudentNotes.js
@@ -1,8 +1,37 @@
 
-export default function StudentNotes() {
+import { useState } from "react"
 
-  return (<div 
+export default function StudentNotes(props) {
+
+  //assign the student prop to a variable
+  const student = props.student
+
+  //create a state for the notes array 
+  const [notes, setNotes] = useState([...student.notes])
+
+  return (<div
     className="Student-notes">
+    <h4>1-on-1 notes</h4>
+
+    <form>
+      <label
+        htmlFor="commentor">Commentor Name</label> 
+      <input
+        name="commentor"
+        id="commentor"></input>
+      <br/>
+
+      <label
+        htmlFor="comment">Comment</label>
+      <input
+        name="comment"
+        id="comment"></input>
+      <br/>
+
+      <button 
+      type="submit"
+      className="note-button">Add Note</button>
+    </form>
 
     {/* when show more is clicked, under details */}
     {/* show form from new notes and all previous notes under the form */}
