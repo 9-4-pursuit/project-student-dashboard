ecf2be5aabdafa85c4bc46aa563c34fde3699aef
arerimr <raydelys.morrobelr@gmail.com>
2023-02-22 19:47:13 -0500

    added on track to graduate label

--- src/App.js
+++ src/App.js
@@ -7,6 +7,48 @@ import data from "./data/data.json"
 function App() {
   const [cohortArr, setCohortArr] = useState([])
   const [studentsData, setStudentsData] = useState(data)
+  const [stuHead, setStuHead] = useState("All Students")
+  const [form, setForm] = useState({
+    commenter: "",
+    comment: ""
+  })
+  const [comments, setComments] = useState([])
+
+  // const [more, setMore] = useState("")
+  // const [moreArr, setMoreArr] = useState([])
+
+  const [defaultStatus, toggleStatus] = useState(true)
+
+  function handleTextChange(event) {
+    event.preventDefault()
+if (event.target.id === "name"){
+  setForm({...form, commenter: event.target.value})
+} else{
+  setForm({...form, comment: event.target.value })
+}
+
+
+  }
+
+  function addComment() {
+
+    studentsData.map((stu) => {
+
+      return (
+
+        setComments([form, ...stu.notes, ...comments])
+
+      )
+
+    })
+
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    addComment()
+    console.log(comments)
+  }
 
   function cohortsMenu() {
     const copyArr = data.map((item) => {
@@ -14,35 +56,61 @@ function App() {
       item.cohortStartDate = new Date(item.cohortStartDate)
       return item
     }).sort((a, b) => b.cohortStartDate - a.cohortStartDate)
-    
+
     const orgArr = copyArr.map((x) => x = x.cohortCode)
-    
+
     setCohortArr([...new Set(orgArr)])
   }
 
-  function filterStudents(value){
+  function filterStudents(value) {
     const studentsArr = [...data]
-    const filteredArr = studentsArr.filter((x)=>x.cohort.cohortCode == value)
+    const filteredArr = studentsArr.filter((x) => x.cohort.cohortCode === value)
 
-    value == "all" ? setStudentsData(studentsArr) :  setStudentsData(filteredArr)
+    value === "all" ? setStudentsData(studentsArr) : setStudentsData(filteredArr)
+    value === "all" ? setStuHead("All Students") : setStuHead(value.substring(0, (value.length - 4)) + " " + value.substring(value.length - 4))
 
-    console.log(studentsData)
+    // console.log(studentsData)
   }
 
-  useEffect(()=>{
+  function settingDob(number) {
+    const stringDate = new Date(number)
+    const finalString = stringDate.toDateString().slice(4, 15)
+    return finalString.substring(0, (finalString.length - 5)) + "," + finalString.substring(finalString.length - 5)
+  }
+
+  function showMore(e) {
+    e.preventDefault()
+    toggleStatus(!defaultStatus)
+  }
+
+  useEffect(() => {
     console.log('useefffect')
     cohortsMenu()
-  },[] )
+    // onTracktoGraduate()
+  }, [])
 
 
   return (
     <div>
       <Header />
+      <button value="1/2/00" onClick={true}>console</button>
       <div className="main">
-      <Cohorts cohortArr={cohortArr} filterStudents={filterStudents}/>
-      <Students data={data} studentsData={studentsData}/>
+        <Cohorts cohortArr={cohortArr} filterStudents={filterStudents} />
+        <Students
+          data={data}
+          studentsData={studentsData}
+          settingDob={settingDob}
+          stuHead={stuHead}
+          defaultStatus={defaultStatus}
+          showMore={showMore}
+          form={form}
+          handleTextChange={handleTextChange}
+          handleSubmit={handleSubmit}
+          comments={comments}
+        // more={more}
+        // setMore={setMore}
+        />
       </div>
-      <button value="i am value" onClick={filterStudents}>console</button>
     </div>
   );
 }
--- src/Components/Students.js
+++ src/Components/Students.js
@@ -1,18 +1,92 @@
 
 
-function Students({data, studentsData}){
-    return(
+function Students({ data, studentsData, settingDob, stuHead, defaultStatus, showMore, form, handleTextChange, handleSubmit, comments }) {
+    let more;
+
+    defaultStatus ? more = "Show More" : more = "Show Less"
+
+    return (
         <div className="students">
-            <h2>All Students</h2>
-            <p>Total Students: {studentsData.length}</p>
-            <ul>
+            <h2>{stuHead}</h2>
+            <p>Total Students: <span className="green">{studentsData.length}</span></p>
+            <ul id="mainUl">
                 {
-                    studentsData.map((stu)=>{
-                        return(
-                        <li key={stu.id}>
-                            <img src={stu.profilePhoto} alt={stu.names.preferredName}/>
-                            <h4>{stu.names.preferredName} {stu.names.middleName.charAt(0)}. {stu.names.surname}</h4>
-                        </li>
+                    studentsData.map((stu) => {
+                        return (
+                            <li id="mainLi" key={stu.id}>
+
+                                <img src={stu.profilePhoto} alt={stu.names.preferredName} />
+
+                                <p>
+                                    <strong>{stu.names.preferredName} {stu.names.middleName.charAt(0)}. {stu.names.surname}</strong>
+                                    <br />
+                                    {stu.username}
+                                    <br />
+                                    <span className="green">Birthday: </span>{settingDob(stu.dob)}
+                                </p>
+                                <div className="green">
+                                    {Object.values(stu.certifications).every(item => item === true) && stu.codewars.current.total > 600 ? "On Track to Graduate" : ""}
+                                </div>
+                                <a onClick={(e) => showMore(e)} className="green" href="true" value={more}>{more}...</a>
+
+
+                                <div className="moreBlock" style={defaultStatus ? { display: "none" } : { display: "block" }}>
+                                    <p>
+                                        <div>
+                                            <strong>Codewars:</strong>
+                                            <p><span className="green">Current Total:</span> {stu.codewars.current.total}</p>
+                                            <p><span className="green">Last Week:</span> {stu.codewars.current.lastWeek}</p>
+                                            <p><span className="green">Goal:</span> {stu.codewars.goal.total}</p>
+                                            <p><span className="green">Percent of Goal Achieved:</span> {((stu.codewars.current.total / stu.codewars.goal.total) * 100).toFixed(0)}%</p>
+                                        </div>
+                                        <div>
+                                            <strong>Scores</strong>
+                                            <p><span className="green">Assignments:</span> {stu.cohort.scores.assignments * 100} %</p>
+                                            <p><span className="green">Projects:</span> {stu.cohort.scores.projects * 100} %</p>
+                                            <p><span className="green">Assessments:</span> {stu.cohort.scores.assessments * 100} %</p>
+                                        </div>
+                                        <div>
+                                            <strong>certifications</strong>
+                                            <p><span className="green">Resume:</span> {stu.certifications.resume ? "✔" : "❌"}</p>
+                                            <p><span className="green">Linkedin:</span> {stu.certifications.linkedin ? "✔" : "❌"}</p>
+                                            <p><span className="green">Mock Interview:</span> {stu.certifications.mockInterview ? "✔" : "❌"}</p>
+                                            <p><span className="green">GitHub:</span> {stu.certifications.github ? "✔" : "❌"}</p>
+                                        </div>
+
+                                    </p>
+                                    <hr></hr>
+                                    <strong>1-on-1 Notes</strong>
+                                    <div className="oneBox">
+                                        <form>
+                                            <label>Commenter Name
+                                                <input onChange={(e)=>handleTextChange(e)} type="text" value={form.commenter} id="name"/>
+                                            </label>
+                                            <br/>
+                                            <br/>
+                                            <label>Comment
+                                                <input onChange={(e)=>handleTextChange(e)} type="text" value={form.comment} id="comment"/>
+                                            </label>
+                                            <br/>
+                                            <br/>
+                                            <button onClick={(e)=>handleSubmit(e)}>Add Note</button>
+                                        </form>
+                                    </div>
+                                    <ul className="oneList">
+                                        {
+                                            comments.map((com)=>{
+                                                return(
+                                                    <li>{com.commenter} says, "{com.comment}"</li>
+
+                                                )
+                                            })
+                                        }
+
+                                    </ul>
+                                </div>
+                                {/* <div className="green" style={defaultStatus ? { display: "none" } : { display: "block" }}>
+                                    On Track to Graduate
+                                </div> */}
+                            </li>
                         )
                     })
                 }
