362dcbc6650e37b7ca53a81e7d3423c1886da5e1
Diegocarrero <diegocar2413@gmail.com>
2023-02-24 18:09:49 -0500

    Display On Track to Graduate conditionally

--- src/components/StudentCard.js
+++ src/components/StudentCard.js
@@ -7,12 +7,24 @@ export default function StudentCard( { student }) {
     month: 'long',
     day: 'numeric'
   })
+
+  function OnTrack({ resume, linkedin, mockInterview, codewars }) {
+    if (resume === true && linkedin === true && mockInterview === true && codewars > 600) {
+      return <p>On Track to Graduate</p>
+    }
+  }
   
 
   return (
     <div className="card">
       {/* <img src={student.profilePhoto} alt=''/> */}
       <h6><strong>{student.names.preferredName} {student.names.middleName[0].toUpperCase()}. {student.names.surname}</strong></h6>
+      <OnTrack
+        resume={student.certifications.resume}
+        linkedin={student.certifications.linkedin}
+        mockInterview={student.certifications.mockInterview}
+        codewars={student.codewars.current.total}
+      />
       <p>{student.username}</p>
       <p>Birthday: {dateFormatting.format(new Date(student.dob))}</p>
       <br></br>
