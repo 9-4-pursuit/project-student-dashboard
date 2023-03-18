f3fa105a47779a39fb3df664336c9b16131d26c9
laura-williams-1 <laurawilliams@pursuit.org>
2023-03-17 22:10:07 -0400

    all student list features completed

--- src/Components/StudentCard.js
+++ src/Components/StudentCard.js
@@ -6,13 +6,15 @@ export default function StudentCard({
   email,
   studentPhoto,
   birthDay,
+  formatDate,
 }) {
   console.log(firstName);
   return (
     <div>
+      <img src={studentPhoto} />
       <h1>{firstName + " " + middleName[0] + ". " + lastName}</h1>
       <p>{email}</p>
-      <p>Birthday:{birthDay}</p>
+      <p>Birthday: {formatDate(birthDay)}</p>
     </div>
   );
 }
--- src/Components/StudentList.jsx
+++ src/Components/StudentList.jsx
@@ -1,12 +1,25 @@
 import StudentCard from "./StudentCard";
 const StudentList = ({ students, setStudents }) => {
+  function formatDate(dateString) {
+    const dateParts = dateString.split("/");
+    const year = dateParts[2];
+    const month = dateParts[0] - 1;
+    const day = dateParts[1];
+    const dateObj = new Date(year, month, day);
+    const formattedDate = dateObj.toLocaleDateString("en-US", {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    });
+    return formattedDate;
+  }
   return (
     <div>
       <h1>All Students</h1>
+      <p>Total Students: {students.length}</p>
       {students.map((student) => {
         return (
           <StudentCard
-            student={student}
             firstName={student.names.preferredName}
             middleName={student.names.middleName}
             lastName={student.names.surname}
@@ -14,6 +27,7 @@ const StudentList = ({ students, setStudents }) => {
             birthDay={student.dob}
             studentPhoto={student.profilePhoto}
             key={student.id}
+            formatDate={formatDate}
           />
         );
       })}
