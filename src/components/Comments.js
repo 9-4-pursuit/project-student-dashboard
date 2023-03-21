6b74eca9f39eb7f3b8ce05fe2e9428af3706e238
MichelBDLC <michelbdlc@gmail.com>
2023-03-20 23:05:08 -0700

    deleted unused function

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,15 +1,17 @@
 export default function Comments() {
 
+
+
     return (
         <section>
             <br />
             <strong> 1-on-1 Notes </strong>
             <form>
                     <label> Commenter Name: </label>
-                    <input type="text" onKeyUp={handleCommenter} />
+                    <input type="text" />
                     <br></br>
                     <label> The Comment:  </label>
-                    <input type="text" onKeyUp={handleComment} />
+                    <input type="text" />
                     <br></br>
                     <button type="submit"> Add Comment </button>
             </form>
